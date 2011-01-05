50fc5afb83bf3d11219cbd95f092516f0d409974
Alexander Korotykin <alexander.korotykin@gmail.com>
2011-01-05 23:05:01 +0300

    refactoring

--- Del2RecVC.cpp
+++ Del2RecVC.cpp
@@ -8,51 +8,52 @@
 #include <stdlib.h>
 #include <vector>
 
+void print_help()
+{
+	::printf("\nDeleting one or more files to Recycle Bin"
+		"\nUsage: Del2Rec [disk:][path]file_name\n");
+}
 
 int main(const int argc, const char* const argv[])
 {
-	int res = 0;
-	int l = 0;
-	for(int i = 1; i < argc; i++)
+	if (argc <= 1) {
+		::print_help();
+		return 0;
+	}
+	int length = 0;
+	for(int i = 1; i < argc; ++i)
 	{
 		if((argv[i][0] == '-') || (argv[i][0] == '/')) continue; // skip options
 		char * absPath = ::_fullpath(NULL, argv[i], 0);
 		if(absPath == 0) continue; // error - wrong full path
-		l += ::strlen(absPath);
+		length += ::strlen(absPath);
 		::free(absPath);
-		++l;
+		++length;
 	}
-	if(l > 0)
+	if (length == 0) {
+		::print_help();
+		return 1;
+	}
+	++length;
+	std::vector<char> files_buf(length);
+	char *fbp = &files_buf[0];
+	for(int i = 1; i < argc; ++i)
 	{
-		++l;
-		//char *ff = new char[l];
-		std::vector<char> files_buf(l);
-		char *_f = &files_buf[0];
-		for(int i = 1; i < argc; i++)
-		{
-			if((argv[i][0] == '-') || (argv[i][0] == '/')) continue;
-			char * absPath = ::_fullpath(NULL, argv[i], 0);
-			if(absPath == 0) continue;
-			for(const char *p = absPath; *p != 0; ++p, ++_f) *_f = *p; // strcpy ?
-			::free(absPath);
-			*_f = 0;
-			++_f;
-		}
-		*_f = 0;
-		SHFILEOPSTRUCT arg_fo;
-		arg_fo.hwnd = 0;
-		arg_fo.wFunc = FO_DELETE;
-		arg_fo.pFrom = &files_buf[0];
-		arg_fo.pTo = 0;
-		arg_fo.fFlags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION | FOF_SILENT | FOF_NOERRORUI;
-		res = ::SHFileOperation(&arg_fo);
-		//delete[] ff;
+		if((argv[i][0] == '-') || (argv[i][0] == '/')) continue;
+		char * absPath = ::_fullpath(NULL, argv[i], 0);
+		if(absPath == 0) continue;
+		for(const char *p = absPath; *p != 0; ++p, ++fbp) *fbp = *p; // strcpy is not sutable
+		::free(absPath);
+		*fbp = 0;
+		++fbp;
 	}
-	else
-	{
-		::printf("\nDeleting one or more files to Recycle Bin"
-			"\nUsage: Del2Rec [disk:][path]file_name");
-	}
-	return res;
+	*fbp = 0;
+	SHFILEOPSTRUCT arg_fo;
+	arg_fo.hwnd = 0;
+	arg_fo.wFunc = FO_DELETE;
+	arg_fo.pFrom = &files_buf[0];
+	arg_fo.pTo = 0;
+	arg_fo.fFlags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION | FOF_SILENT | FOF_NOERRORUI;
+	return ::SHFileOperation(&arg_fo);
 }
 
